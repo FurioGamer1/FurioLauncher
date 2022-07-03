16e320f640fa328b5aa50607b7dfa2dd1b42c15a
FurioGamer <68191640+FurioGamer1@users.noreply.github.com>
2022-07-03 11:46:22 +0200

    Update index.js

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -22,9 +22,10 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n' ai pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "Je... vie...", "author": "FurioGamer" },
+            { "message": "Salut je suis du code.", "author": "FurioGamer" },
+            { "message": "Linux n' ai pas un os, mais un kernel.", "author": "FurioGamer" },
+            { "message": "Je marche...", "author": "FurioGamer"}
         ];
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
